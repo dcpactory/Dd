d4f97f63bf1d28a295fdc12d0796c3ff1d28de3b
dcpactory <wathidunethyuga@gmail.com>
2025-11-24 17:15:02 +0530

    Create Viewpager.js

---
+++ Viewpager.js
@@ -0,0 +1,7 @@
+{
+  "images": [
+    "https://images.pexels.com/photos/34950/pexels-photo.jpg",
+    "https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg",
+    "https://images.pexels.com/photos/210186/pexels-photo-210186.jpeg"
+  ]
+}
